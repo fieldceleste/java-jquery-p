f18ca9fac7de737418c011d1482b3d2570d857bb
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-03-27 17:26:25 -0700

    finish tweaking and modifying quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,24 @@
 $(document).ready(function () {
   $("#questions").submit(function () {
       var nameInput = $("input#name").val();
-      var desktop = parseInt($('input[name = "desktop"]:checked').val());
-      var animal = parseInt($('input[name = "animal"]:checked').val());
-      var programmer = parseInt($("#programmer").val());
-      var sites = parseInt($("#sites").val());
+      var desktop = parseInt($("#desktop").val());
+      var animal = parseInt($("#animal").val());
+      var programmer = parseInt($('input[name = "programmer"]:checked').val());
+      var sites = parseInt($('input[name = "sites"]:checked').val());
       var experience = parseInt($("#experience").val());
+
       var result = desktop + animal + programmer + sites + experience;
 
       
 
       $(".name").text(nameInput);
-      if (result <= 4) {
-          $("#answer").text("C# (sharp), cause you seem to like Windows based products. You will be able to build desktop and mobile applications, and game development!");
-      } else if (result <= 11) {
-          $("#answer").text("Swift, cause you seem to like Apple based products. You will be able to build mobile apps on any IOS (apple) devices and can build desktop applications as well.");
-      } else if (result <= 16) {
-          $("#answer").text("Ruby,so you can build web applicaions, desktop games, and mobile apps!");
+      if (result <= 5) {
+          $("#answer1").text("C# (sharp), cause you seem to like Windows based products. You will be able to build desktop and mobile applications, and game development!");
+      } else if (result <= 10) {
+          $("#answer1").text("Swift, cause you seem to like Apple based products. You will be able to build mobile apps on any IOS (apple) devices and can build desktop applications as well.");
+      } else if (result <= 15) {
+          $("#answer1").text("Ruby,so you can build web applicaions, desktop games, and mobile apps!");
       }
-      $( "submit" ).click(function() {
-        $( "p" ).show( "slow" );
-      });
-
       event.preventDefault();
   });
 });
